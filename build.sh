516272bd3fdbc167132e29b8a6f5283bbc9d3c98
muhammmadnantaa-hub <muhammmadnantaa@gmail.com>
2025-11-01 13:02:36 +0000

    build.sh

--- build.sh
+++ build.sh
@@ -6,9 +6,12 @@ set -e
 KERNEL_PATH=out/arch/arm64/boot
 
 # Set kernel name
-BUILD_TYPE="ALPHA"
+BUILD_TYPE="SUKISU"
 DATE="$(TZ=Asia/Jakarta date +%Y%m%d%H%M%S)"
-KERNEL_NAME="rethinking${BUILD_TYPE}-${DATE}.zip"
+KERNEL_NAME="SukiSuA16${BUILD_TYPE}-${DATE}.zip"
+
+# Clone SukiSU repo
+if [ ! -d "KernelSU" ]; then curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s susfs-main; fi
 
 function KERNEL_COMPILE() {
 	if [ "$1" == "install" ]; then
@@ -19,7 +22,7 @@ function KERNEL_COMPILE() {
 	# Set environment variables
 	export USE_CCACHE=1
 	export KBUILD_BUILD_HOST=gcorprjkt
-	export KBUILD_BUILD_USER=noevdvm
+	export KBUILD_BUILD_USER=noevdev
 
 	# Create output directory and do a clean build
 	rm -rf out && mkdir -p out
@@ -91,9 +94,12 @@ function KERNEL_RESULT() {
 		exit 1
 	fi
 
-	# Create AnyKernel
-	rm -rf AnyKernel
-	git clone --depth=1 https://github.com/muhammmadnantaa-hub/AnyKernel.git anykernel
+	# Apply kpm
+	KERNEL_PATCH
+
+	# Create anykernel
+	rm -rf anykernel
+	git clone https://github.com/sarthakroy2002/AnyKernel3.git anykernel
 
 	# Copying image
 	cp ${KERNEL_PATH}/Image.gz-dtb anykernel/
