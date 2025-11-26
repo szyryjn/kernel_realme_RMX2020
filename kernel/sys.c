0a2bce75fdca99ac0703d182344649ea70177d13
szyryjn <amarkumarray060@gmail.com>
2025-11-26 10:53:18 +0530

    FIXED COMPILATION ERRORS

--- kernel/sys.c
+++ kernel/sys.c
@@ -1188,11 +1188,6 @@ static int override_release(char __user *release, size_t len)
 #ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
 extern void susfs_spoof_uname(struct new_utsname* tmp);
 #endif
-}
-
-#ifdef CONFIG_KSU_SUSFS_SPOOF_UNAME
-extern void susfs_spoof_uname(struct new_utsname* tmp);
-#endif
 SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 {
 	struct new_utsname tmp;
